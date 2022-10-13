be5629d7baf6e220b1ff3c87d417326f9738e45a
陈冰 <cb2@meitu.com>
2022-10-13 09:44:20 +0800

    【Feature】用户通知：远程推送配置成功

--- DemoUserNotification/DemoUserNotification/DemoUserNotificationApp.swift
+++ DemoUserNotification/DemoUserNotification/DemoUserNotificationApp.swift
@@ -19,16 +19,33 @@ struct DemoUserNotificationApp: App {
     }
 }
 
-
-
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         UNUserNotificationCenter.current().requestAuthorization(
             options: [.alert, .badge, .sound]) { (granted, error) in
-            print("是否授权: \(granted)")
-            // Enable or disable features based on authorization.
-        }
+                print("是否授权: \(granted)")
+                DispatchQueue.main.async {
+                    // 注册远程通知
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
         return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // 接收到 deviceToken
+        let token = deviceToken.map { data -> String in
+            return String(format: "%02.2hhx", data)
+        }.joined()
+        print("Device Token: \(token)")
+        // 转发 deviceToken 给后端服务器，能够启用远程通知功能
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("无法注册远程通知失败 \(error)")
+        // 禁用远程通知功能
+        // 模拟器不可用远程通知
+        // 需要在 Signing&Capacities 添加 Push Notifications 功能才能使用，否则提示 Error Domain=NSCocoaErrorDomain Code=3000 "未找到应用程序的“aps-environment”的授权字符串" UserInfo={NSLocalizedDescription=未找到应用程序的“aps-environment”的授权字符串}
     }
 }
 
