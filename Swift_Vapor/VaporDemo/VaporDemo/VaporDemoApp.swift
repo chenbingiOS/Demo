dcc4bc371f5bfd4f01811edcef5da198e38e8752
陈冰 <cb2@meitu.com>
2022-11-06 09:58:55 +0800

    【Feature】Vapor：本地运行成功

---
+++ Swift_Vapor/VaporDemo/VaporDemo/VaporDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaporDemoApp.swift
+//  VaporDemo
+//
+//  Created by mtAdmin on 2022/11/5.
+//
+
+import SwiftUI
+
+@main
+struct VaporDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
